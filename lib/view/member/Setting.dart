bdbb79abb9744eea21f3b2358940938cc2758f28
etiosa obasuyi <obasuyietiosa@gmail.com>
2021-09-02 15:21:32 -0400

    setting page done

--- lib/view/member/Setting.dart
+++ lib/view/member/Setting.dart
@@ -21,44 +21,68 @@ class _SettingState extends State<Setting> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Icon(Icons.arrow_back),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30.0, left: 20),
+                child: Icon(Icons.arrow_back, 
+                
+                size: 40,),
+              ),
               Padding(
                 padding: const EdgeInsets.only(left:20.0, right:20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Account'),
-                    ElevatedButton(
-                        onPressed: () {
-                          print('update');
-                        },
-                        child: Text('Update'))
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom:50.0, left: 10, right: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Account',
+                        style: TextStyle(fontWeight: FontWeight.bold,
+                          fontSize: 30
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('update');
+                          },
+                          child: Text('Update'))
+                    ],
+                  ),
                 ),
               ),
             
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-               Container(
-                        height: 120,
-                        width: 120,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: new DecorationImage(
-                                image: new AssetImage('assets/images/House.png'),
-                                fit: BoxFit.cover)),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                      Text('John Joe'), 
-                      Text('Joe@gmail.com'),
-                      Text('111-333-4444')
-                      ],)
+            Padding(
+              padding: const EdgeInsets.only(bottom:40.0, left:10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                 Container(
+                          height: 120,
+                          width: 120,
+                          margin: EdgeInsets.only(right:50),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  image: new AssetImage('assets/images/House.png'),
+                                  fit: BoxFit.cover)),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                        Text('John Joe',
+                          style: TextStyle(fontSize: 20,
+                                    height: 1.5
+                          ),
+                        ), 
+                        Text('Joe@gmail.com',
+                              style: TextStyle(fontSize: 20, height: 1.5),
+                            ),
+                        Text('111-333-4444',
+                              style: TextStyle(fontSize: 20, height: 1.5),
+                            )
+                        ],)
           
         
-            ],),
+              ],),
+            ),
             
             ElevatedButton(onPressed: (){print('change picture');}, 
             child:Text('change picture'),
